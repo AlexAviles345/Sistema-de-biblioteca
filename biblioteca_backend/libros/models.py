f0ebfd791e5bf00934122916e9b9c99b975c57a9
AlexAviles345 <20233tn198@utez.edu.mx>
2026-05-07 09:41:55 -0600

    [ADD] Modelo de libro en backend

--- biblioteca_backend/libros/models.py
+++ biblioteca_backend/libros/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import base64
 
 # Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    paginas = models.IntegerField()
+    editorial = models.CharField(max_length=50)
+
+    # CAMPO PARA GUARDAR LA IMAGEN EN EL BACK
+    # upload_to='imagenes-libros/' creará una carpeta llamada "imagenes-libros" dentro del directorio "media"
+    foto = models.ImageField(upload_to='imagenes-libros/', blank=True, null=True)
+    

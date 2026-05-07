e547c5736641bb0d3e240cf07c5363c5257354cc
AlexAviles345 <20233tn198@utez.edu.mx>
2026-05-07 09:47:54 -0600

    [ADD] ViewSet para exportar todas las operaciones crud del modelo de libros en backend

--- biblioteca_backend/libros/views.py
+++ biblioteca_backend/libros/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Libro 
+from .serializers import LibroSerializer
+
+# Encargado de manejar las operaciones CRUD para el modelo Libro
+class LibroViewSet(viewsets.ModelViewSet):
+    queryset = Libro.objects.all()
+    serializer_class = LibroSerializer
